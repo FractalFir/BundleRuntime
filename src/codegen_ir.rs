0180395e8b2c84cd77a12d52570e21521fda6ad8
FractalFir <fractalfirdev@gmail.com>
2022-11-23 17:49:57 +0100

    Changed codegen IR to better reflect instructoins for CPU-s. Added new Mov instruction and a new optimalization related to it.

--- src/codegen_ir.rs
+++ src/codegen_ir.rs
@@ -1,7 +1,8 @@
+/// TODO: rethink the use of word "alias" in code(it is not accurate depiction of what some function do and may be misleading).
 #[derive(Debug,PartialEq,Clone,Copy)]
 pub enum IrVar{
-    Arg(u8),
-    Tmp(u8),
+//   Arg(u8),
+    IReg(u8),
 }
 impl IrVar{
     fn alias(&mut self,var:IrVar,alias:IrVar){
@@ -11,8 +12,9 @@ impl IrVar{
 impl Eq for IrVar{}
 #[derive(Debug,Clone,Copy,PartialEq)]
 pub enum IrOp{
-    Add(IrVar,IrVar,IrVar),
-    Mul(IrVar,IrVar,IrVar),
+    Add(IrVar,IrVar),
+    Mul(IrVar,IrVar),
+    Mov(IrVar,IrVar),
     Pop(IrVar),
     Push(IrVar),
     Return(IrVar),
@@ -20,38 +22,63 @@ pub enum IrOp{
 }
 impl Eq for IrOp{}
 impl IrOp{
+    /// Replaces internal references to var with an alias.
     fn alias_variable(&mut self,var:IrVar,alias:IrVar){
         match self{
-            IrOp::Add(ref mut a,ref mut b,ref mut dst)=>{
-                a.alias(var,alias);
-                b.alias(var,alias);
-                dst.alias(var,alias);
+            IrOp::Add(ref mut val,ref mut addend)=>{
+                val.alias(var,alias);
+                addend.alias(var,alias);
             },
-            IrOp::Mul(ref mut a,ref mut b,ref mut dst)=>{
-                a.alias(var,alias);
-                b.alias(var,alias);
-                dst.alias(var,alias);
+            IrOp::Mul(ref mut multiplicand,ref mut multiplier)=>{
+                multiplicand.alias(var,alias);
+                multiplier.alias(var,alias);
+            },
+            IrOp::Mov(ref mut source,ref mut target)=>{
+                source.alias(var,alias);
+                target.alias(var,alias);
             },
             IrOp::Push(ref mut src)=>src.alias(var,alias),
             IrOp::Pop(ref mut dst)=>dst.alias(var,alias),
             IrOp::Return(ref mut ret)=>ret.alias(var,alias),
             IrOp::Nop=>(), //No variables, ignore
-            _=>todo!("{self:?} variant is not supported by varaible aliaser!"),
+            _=>todo!("{self:?} variant is not supported by variable aliaser!"),
         }
     }
+    /// Checks if a variable is used by this instruction
+    fn uses_variable(&self,var:IrVar)->bool{
+        match self{
+            IrOp::Add(val,addend)=>*val == var || *addend == var,
+            IrOp::Mul(multiplicand,multiplier)=>*multiplicand == var || *multiplier == var,
+            IrOp::Mov(source,target)=>*source == var || *target == var,
+            IrOp::Push(src)=>*src == var,
+            IrOp::Pop(dst)=>*dst == var,
+            IrOp::Return(ret)=>*ret == var,
+            IrOp::Nop=>false, //No variables, ignore
+        }
+    }
+    /// Checks whether this instruction does anything (is not equivalent to a Nop). 
     fn is_meanigfull(&self)->bool{
         match self{
             IrOp::Nop=>false,
+            IrOp::Mov(src,target)=>src != target,
             _=>true,
         }
     }
 }
+/// Replaces one variable with another.
 fn alias_variable(ops:&mut [IrOp],var:IrVar,alias:IrVar){
     for mut op in ops{
         op.alias_variable(var,alias);
     }
 }
-// Tmp is used as destination and swapped with ops. This saves allocations.
+fn is_var_used_after(ops:&Vec<IrOp>,var:IrVar,pos:usize)->bool{
+    let mut iter = ops.iter().skip(pos + 1);
+    while let Some(op) = iter.next(){
+        if op.uses_variable(var){return true};
+    }
+    return false;
+}
+// IReg is used as destination and swapped with ops. This saves allocations.
 fn push_pop_inline(ops:&mut Vec<IrOp>){
    let mut aliases:Vec<(IrVar,IrVar,usize,usize)> = Vec::new(); //add push & pop loc
    let mut op_iter = ops.iter();
@@ -90,12 +117,46 @@ fn push_pop_inline(ops:&mut Vec<IrOp>){
         ops[alias.2] = IrOp::Nop;
         ops[alias.3] = IrOp::Nop;
    } 
-   ops.retain(IrOp::is_meanigfull)
 }
+fn unneded_move_inline(ops:&mut Vec<IrOp>){
+   let mut aliases:Vec<(IrVar,IrVar)> = Vec::new(); //add push & pop loc
+   let mut op_iter = ops.iter();
+   let mut index = 0;
+   while let Some(op) = op_iter.next(){
+        match op{
+            IrOp::Mov(src,dst)=>{
+                if !is_var_used_after(ops,*src,index){
+                    aliases.push((*src,*dst));
+                }
+            },
+            _=>(),
+        };
+        index+=1;
+   }
+   // enable for debug if this function fails after a change to check if aliasing works
+   /*
+   for alias in &aliases{
+        println!("als: {:?} is now {:?} and {:?} and {:?} are gone.",alias.0,alias.1,ops[alias.2],ops[alias.3]); 
+   }
+   */
+   for alias in &aliases{
+        //reverse order of tuple because source (alias.0) becomes overrides target (alias.1)
+        alias_variable(ops,alias.1,alias.0);
+   } 
+}
+/*
+fn sqish_vars(ops:&mut Vec<IrOp>){
+    let vars = Vec::new();
+    for op in ops{
+        
+    }
+}*/
 fn optimize_ir(ops:&mut Vec<IrOp>){
     let mut last_size = ops.len();
     'opt: loop {
         push_pop_inline(ops);
+        unneded_move_inline(ops);
+        ops.retain(IrOp::is_meanigfull);
         let curr_size = ops.len();
         if !(curr_size < last_size){return};
         last_size = curr_size;
@@ -106,32 +167,33 @@ mod test{
     use super::*;
     #[test]
     fn test_opt_stack_simple_ir(){
-        let arg0 = IrVar::Arg(0);
-        let arg1 = IrVar::Arg(1);
-        let tmp0 = IrVar::Tmp(0);
-        let tmp1 = IrVar::Tmp(1);
-        let tmp2 = IrVar::Tmp(2);
-        let tmp3 = IrVar::Tmp(3);
-        let tmp4 = IrVar::Tmp(4);
-        let tmp5 = IrVar::Tmp(5);
-        let tmp6 = IrVar::Tmp(6);
-        let tmp7 = IrVar::Tmp(7);
-        let tmp8 = IrVar::Tmp(8);
-        let ret = IrVar::Tmp(9);
+        let arg0 = IrVar::IReg(0);
+        let arg1 = IrVar::IReg(1);
+        let tmp0 = IrVar::IReg(2);
+        let tmp1 = IrVar::IReg(3);
+        let tmp2 = IrVar::IReg(4);
+        let tmp3 = IrVar::IReg(5);
+        let tmp4 = IrVar::IReg(6);
+        let tmp5 = IrVar::IReg(7);
+        let tmp6 = IrVar::IReg(8);
+        let tmp7 = IrVar::IReg(9);
+        let tmp8 = IrVar::IReg(10);
+        let ret = IrVar::IReg(11);
         let mut code = vec![
             IrOp::Push(arg0),
             IrOp::Push(arg0),
-            IrOp::Pop(tmp0),IrOp::Pop(tmp1),IrOp::Add(tmp0,tmp1,tmp2),IrOp::Push(tmp2),
+            IrOp::Pop(tmp0),IrOp::Pop(tmp1),IrOp::Mov(tmp0,tmp2),IrOp::Mul(tmp2,tmp1),IrOp::Push(tmp2),
             IrOp::Push(arg1),
             IrOp::Push(arg1),
-            IrOp::Pop(tmp3),IrOp::Pop(tmp4),IrOp::Add(tmp3,tmp4,tmp5),IrOp::Push(tmp5),
-            IrOp::Pop(tmp6),IrOp::Pop(tmp7),IrOp::Mul(tmp6,tmp7,tmp8),IrOp::Push(tmp8),
+            IrOp::Pop(tmp3),IrOp::Pop(tmp4),IrOp::Mov(tmp3,tmp5),IrOp::Mul(tmp5,tmp4),IrOp::Push(tmp5),
+            IrOp::Pop(tmp6),IrOp::Pop(tmp7),IrOp::Mov(tmp6,tmp8),IrOp::Add(tmp8,tmp7),IrOp::Push(tmp8),
             IrOp::Pop(ret),IrOp::Return(ret)
             ];
         println!("{}:\n{code:?}\n",code.len());
         //alias_variable(&mut code,tmp0,arg0);
         optimize_ir(&mut code);
-        let expected = [IrOp::Add(IrVar::Arg(0), IrVar::Arg(0), IrVar::Tmp(2)), IrOp::Add(IrVar::Arg(1), IrVar::Arg(1), IrVar::Tmp(5)), IrOp::Mul(IrVar::Tmp(5), IrVar::Tmp(2), IrVar::Tmp(8)), IrOp::Return(IrVar::Tmp(8))];
+        println!("{}:\n{code:?}\n",code.len());
+        let expected = [IrOp::Mul(IrVar::IReg(0), IrVar::IReg(0)), IrOp::Mul(IrVar::IReg(1), IrVar::IReg(1)), IrOp::Add(IrVar::IReg(1), IrVar::IReg(0)), IrOp::Return(IrVar::IReg(1))];
         assert!(*(&code as &[IrOp]) == *(&expected as &[IrOp]),"Generated optimized code {code:?} differs from expected code {expected:?}");
     }
 }
